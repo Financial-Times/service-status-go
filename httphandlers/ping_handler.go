ae4810853c10e1fd55754060295e2b623c5f40d2
Dan Murphy <dan.murphy@ft.com>
2016-02-19 18:08:31 +0000

    Updates to Ping (goLint fix) & Ping test case

--- httphandlers/ping_handler.go
+++ httphandlers/ping_handler.go
@@ -1,9 +1,11 @@
 package httphandlers
 
 import (
+	"fmt"
 	"net/http"
 )
 
+//Ping is a simple handler that always responds with pong as text
 func Ping(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong")
 }
--- httphandlers/ping_handler_test.go
+++ httphandlers/ping_handler_test.go
@@ -1,9 +1,17 @@
 package httphandlers
 
 import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestPingHandler(t *testing.T) {
-
+	req, err := http.NewRequest("GET", "ping", nil)
+	assert.NoError(t, err)
+	w := httptest.NewRecorder()
+	Ping(w, req)
+	assert.Equal(t, w.Code, http.StatusOK)
+	assert.Equal(t, "pong", w.Body.String())
 }
