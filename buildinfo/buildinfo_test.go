3d00b3064bc6969cf7cbbdd4f54868fc45f980c3
Panziera, Luca <luca.panziera@ft.com>
2016-02-22 13:21:28 +0000

    set setUp function and expected variable private in test

--- buildinfo/buildinfo_test.go
+++ buildinfo/buildinfo_test.go
@@ -9,30 +9,31 @@ import (
 	"testing"
 )
 
-var Expected string
+var expected string
 
-func SetUp() {
+func setUp() {
 	Version = "0.0.1"
 	Repository = "github.com/Financial-Times/a-ft-service"
 	Commit = "b1d23060f717364b40a6506f74429f9a290a2b71"
 	Builder = "go version go1.5.2 darwin/amd64"
-	Expected = `{"repository":"github.com/Financial-Times/a-ft-service", "version":"0.0.1", "builder":"go version go1.5.2 darwin/amd64", "dateTime":"` +
+	expected = `{"repository":"github.com/Financial-Times/a-ft-service", "version":"0.0.1", "builder":"go version go1.5.2 darwin/amd64", "dateTime":"` +
 		fmt.Sprintf("%d%02d%02d%02d%02d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute()) + `", "commit":"b1d23060f717364b40a6506f74429f9a290a2b71"}`
 
 }
 
 func TestModel(t *testing.T) {
-	SetUp()
+	setUp()
 	result, err := json.Marshal(GetBuildInfo())
 	assert.NoError(t, err)
-	assert.JSONEq(t, Expected, string(result))
+	assert.JSONEq(t, expected, string(result))
 }
 
 func TestBuildInfoHandler(t *testing.T) {
+	setUp()
 	req, err := http.NewRequest("GET", "build-info", nil)
 	assert.NoError(t, err)
 	w := httptest.NewRecorder()
 	BuildInfoHandler(w, req)
 	assert.Equal(t, w.Code, http.StatusOK)
-	assert.JSONEq(t, Expected, w.Body.String())
+	assert.JSONEq(t, expected, w.Body.String())
 }
