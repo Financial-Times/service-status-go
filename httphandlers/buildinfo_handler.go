5beef6f66798d3af52280edbb247efae4d3519ed
Panziera, Luca <luca.panziera@ft.com>
2016-02-24 16:41:21 +0000

    added Content-Type header to build-info handler

--- httphandlers/buildinfo_handler.go
+++ httphandlers/buildinfo_handler.go
@@ -8,6 +8,7 @@ import (
 
 // BuildInfoHandler uses the buildinfo.GetBuildInfo to respond to requests for build info !
 func BuildInfoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	if err := json.NewEncoder(w).Encode(buildinfo.GetBuildInfo()); err != nil {
 		panic(err)
 	}
