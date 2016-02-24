4d6f6af6d093254810ffa3ccba3898e7747f08a5
Dan Murphy <dan.murphy@ft.com>
2016-02-24 15:01:21 +0000

    Expect the default messages from httphandler

--- httphandlers/httphandlers_test.go
+++ httphandlers/httphandlers_test.go
@@ -1,7 +1,6 @@
 package httphandlers
 
 import (
-	//"github.com/Financial-Times/service-status-go/buildinfo"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -18,11 +17,18 @@ func TestPingHandler(t *testing.T) {
 }
 
 func TestBuildInfoHandler(t *testing.T) {
-	// buildinfo.SetUp()
-	// req, err := http.NewRequest("GET", "build-info", nil)
-	// assert.NoError(t, err)
-	// w := httptest.NewRecorder()
-	// BuildInfoHandler(w, req)
-	// assert.Equal(t, w.Code, http.StatusOK)
-	// assert.JSONEq(t, buildinfo.Expected, w.Body.String())
+	expected := `
+        {
+          "version": "Version  is not a semantic version",
+          "repository": "Repository  does not match regex ((git|ssh|http(s)?)|(git@[\\w\\.]+))(:(//)?)([\\w\\.@\\:/\\-~]+)(\\.git)(/)?",
+          "revision": "Revision  does not match regex ^[0-9a-f]{5,40}$",
+          "builder": "",
+          "dateTime": "dateTime  does not match regex ^[0-9]{14}"
+        }`
+	req, err := http.NewRequest("GET", "build-info", nil)
+	assert.NoError(t, err)
+	w := httptest.NewRecorder()
+	BuildInfoHandler(w, req)
+	assert.Equal(t, w.Code, http.StatusOK)
+	assert.JSONEq(t, expected, w.Body.String())
 }
