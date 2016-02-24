035e9856565958d87db5456764411ce271caafad
Dan Murphy <dan.murphy@ft.com>
2016-02-24 09:35:38 +0000

    Update main to print versioninfo as json

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/Financial-Times/service-status-go/buildinfo"
 )
 
 func main() {
-	info := buildinfo.GetBuildInfo()
-	fmt.Printf("Build info is %v+", info)
+	if buildInfo, err := json.Marshal(buildinfo.GetBuildInfo()); err == nil {
+		fmt.Printf("%s", string(buildInfo))
+	}
 }
