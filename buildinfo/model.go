5c71caf7fab1ac3fce67eb2ed7967edc912aeb31
Dan Murphy <dan.murphy@ft.com>
2016-02-24 09:36:42 +0000

    tweak parsers

--- buildinfo/model.go
+++ buildinfo/model.go
@@ -8,15 +8,15 @@ import (
 
 var version string
 var repository string
-var commit string
+var revision string
 var builder string
 var dateTime string
 
 // BuildInfo structure
 type BuildInfo struct {
-	Repository string `json:"repository"`
-	Commit     string `json:"commit"`
 	Version    string `json:"version"`
+	Repository string `json:"repository"`
+	Revision   string `json:"revision"`
 	Builder    string `json:"builder"`
 	DateTime   string `json:"dateTime"`
 }
@@ -27,8 +27,8 @@ func GetBuildInfo() BuildInfo {
 	if err := parseRepository(); err != nil {
 		repository = err.Error()
 	}
-	if err := parseCommit(); err != nil {
-		commit = err.Error()
+	if err := parseRevision(); err != nil {
+		revision = err.Error()
 	}
 	if err := parseVersion(); err != nil {
 		version = err.Error()
@@ -37,32 +37,34 @@ func GetBuildInfo() BuildInfo {
 		dateTime = err.Error()
 	}
 
-	return BuildInfo{repository, commit, version, builder, dateTime}
+	return BuildInfo{version, repository, revision, builder, dateTime}
 }
 
 // currently suport https repositories
-const urlMatch = "^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?$"
+const repositorylMatch = "((git|ssh|http(s)?)|(git@[\\w\\.]+))(:(//)?)([\\w\\.@\\:/\\-~]+)(\\.git)(/)?"
+
+//const repositorylMatch = "^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?$"
 
 // currently needs to be a sha1 (ala git)
-const commitMatch = "^[0-9a-f]{5,40}$"
+const revisionMatch = "^[0-9a-f]{5,40}$"
 
 // variant of the iso-8601 standard (i.e. without the seperators)
 const dateTimeMatch = "^[0-9]{14}"
 
-var urlRegex = regexp.MustCompile(urlMatch)
-var commitRegex = regexp.MustCompile(commitMatch)
+var repositoryRegex = regexp.MustCompile(repositorylMatch)
+var revisionRegex = regexp.MustCompile(revisionMatch)
 var dateTimeRegex = regexp.MustCompile(dateTimeMatch)
 
 func parseRepository() error {
-	if !urlRegex.MatchString(repository) {
-		return fmt.Errorf("Repository %s does not match regex %s", repository, urlMatch)
+	if !repositoryRegex.MatchString(repository) {
+		return fmt.Errorf("Repository %s does not match regex %s", repository, repositorylMatch)
 	}
 	return nil
 }
 
-func parseCommit() error {
-	if !commitRegex.MatchString(commit) {
-		return fmt.Errorf("Commit %s does not match regex %s", commit, commitMatch)
+func parseRevision() error {
+	if !revisionRegex.MatchString(revision) {
+		return fmt.Errorf("Revision %s does not match regex %s", revision, revisionMatch)
 	}
 	return nil
 }
