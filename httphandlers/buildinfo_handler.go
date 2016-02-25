f36fe11516cb3a5ee4eefc2101c8955946072cd5
Panziera, Luca <luca.panziera@ft.com>
2016-02-25 11:26:45 +0000

    Added go documentation

--- httphandlers/buildinfo_handler.go
+++ httphandlers/buildinfo_handler.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 )
 
-// BuildInfoHandlerFunc uses the buildinfo.GetBuildInfo to respond to requests for build info !
+//BuildInfoHandlerFunc is a HandlerFunc that returns a JSON representation of the build-info.
 func BuildInfoHandlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	if err := json.NewEncoder(w).Encode(buildinfo.GetBuildInfo()); err != nil {
@@ -16,6 +16,8 @@ func BuildInfoHandlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func AppendBuildInfo(r *mux.Router) {
+//AppendBuildInfoHandler appends the BuildInfoHandler to a gorilla/mux Router.
+//It enforces to respond to HTTP GETs only.
+func AppendBuildInfoHandler(r *mux.Router) {
 	r.Path("/build-info").Handler(handlers.MethodHandler{"GET": http.HandlerFunc(BuildInfoHandlerFunc)})
 }
