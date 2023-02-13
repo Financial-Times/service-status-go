af85ee045764995e18b613862e0069414c6ebfdc
atanasdinov <atanas.dinov@ft.com>
2023-02-13 19:51:49 +0200

    Simplify GTG implementation

--- gtg/good_to_go.go
+++ gtg/good_to_go.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	timeoutMessage = "Timeout running status check"
-	timeout        = 3
+	timeout        = 3 * time.Second
 )
 
 // Status is the result of running a checker, if the service is GoodToGo then it can serve requests.
@@ -23,32 +23,31 @@ type Status struct {
 type StatusChecker func() Status
 
 // FailFastSequentialChecker composes multiple checkers into one that are executed in sequence. Execution stops as soon as on checker fails.
-func FailFastSequentialChecker(checkers []StatusChecker) (checker StatusChecker) {
-	f := func() Status {
-		for i := range checkers {
-			status := checkers[i].RunCheck()
-			if !status.GoodToGo {
+func FailFastSequentialChecker(checkers []StatusChecker) StatusChecker {
+	return func() Status {
+		for _, checker := range checkers {
+			if status := checker.RunCheck(); !status.GoodToGo {
 				return status
 			}
 		}
-		status := Status{
+
+		return Status{
 			GoodToGo: true,
 			Message:  "OK",
 		}
-		return status
 	}
-	return f
 }
 
 // FailAtEndSequentialChecker composes multiple checkers into one that are executed in sequence. All checkers are executed.
-func FailAtEndSequentialChecker(checkers []StatusChecker) (checker StatusChecker) {
-	f := func() Status {
+func FailAtEndSequentialChecker(checkers []StatusChecker) StatusChecker {
+	return func() Status {
 		result := Status{
 			GoodToGo: true,
 			Message:  "OK",
 		}
-		for i := range checkers {
-			status := checkers[i].RunCheck()
+
+		for _, checker := range checkers {
+			status := checker.RunCheck()
 			if !status.GoodToGo {
 				result.GoodToGo = false
 				if result.Message == "OK" {
@@ -60,20 +59,17 @@ func FailAtEndSequentialChecker(checkers []StatusChecker) (checker StatusChecker
 		}
 		return result
 	}
-	return f
 }
 
 // FailFastParallelCheck creates a composite checker that will run all checkers simultaneously.
 // As soon as any of the checkers fail then the other checkers are ignored.
 func FailFastParallelCheck(checkers []StatusChecker) StatusChecker {
-	fn := func() Status {
+	return func() Status {
 		statusChannel := make(chan Status, len(checkers))
-		for idx := range checkers {
-			check := checkers[idx]
-			go func() {
-				status := check()
-				statusChannel <- status
-			}()
+		for _, checker := range checkers {
+			go func(checker StatusChecker) {
+				statusChannel <- checker()
+			}(checker)
 		}
 		for range checkers {
 			select {
@@ -85,7 +81,6 @@ func FailFastParallelCheck(checkers []StatusChecker) StatusChecker {
 		}
 		return Status{GoodToGo: true}
 	}
-	return fn
 }
 
 // RunCheck executes a checker and returns the result as a status
@@ -101,7 +96,10 @@ func (check StatusChecker) RunCheck() Status {
 	select {
 	case status := <-statusChannel:
 		return status
-	case <-time.After(time.Second * time.Duration(timeout)):
-		return Status{GoodToGo: false, Message: timeoutMessage}
+	case <-time.After(timeout):
+		return Status{
+			GoodToGo: false,
+			Message:  timeoutMessage,
+		}
 	}
 }
